9b94f224407ac64137cd5b58474079558defa988
Bryan <bryan.riveracv@gmail.com>
2023-11-05 14:08:24 +0100

    feat (React-calculator) basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css'
+import styles from "./App.module.css"
+
+
 const  App = () => {
   
   return (
-    <div>
-      Your calculator here
-    </div>
+    <>
+      <div className={styles.calcontainer}>
+        <div className={styles.display}>
+          pantalla
+        </div>
+          
+        <div className={styles.buttons}>
+          <div className={styles.operators}>
+          <button>+</button>
+          <button>-</button>
+          <button>&times;</button>
+          <button>&divide;</button>
+          </div>
+          
+          <div className={styles.lowerbuttons}>
+            
+            <div className={styles.operands}>
+              
+            </div>
+            <div className={styles.equalbutton}>
+              igual
+            </div>
+          </div>
+        </div>
+
+      </div>  
+    </>
   
   );
 }
